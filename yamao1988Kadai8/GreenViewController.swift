24dc7f764d465f6cc64e38579e4b242d8341502e
akio0911 <akio0911@gmail.com>
2023-11-21 23:48:58 +0900

    feedback

--- yamao1988Kadai8/GreenViewController.swift
+++ yamao1988Kadai8/GreenViewController.swift
@@ -6,19 +6,18 @@ class GreenViewController: UIViewController {
     @IBOutlet weak private var label: UILabel!
     @IBOutlet weak private var slider: UISlider!
    
-    let delegate = UIApplication.shared.delegate as! AppDelegate
-    
+    private let delegate = UIApplication.shared.delegate as! AppDelegate
+
     //スライダーの値をラベルに表示
-    @IBAction func changeSlider(_ sender: Any) {
-        self.label.text = "\(self.slider.value)"
-        delegate.shareData = self.slider.value
-        print(self.slider.value)
+    @IBAction private func changeSlider(_ sender: Any) {
+        label.text = "\(slider.value)"
+        delegate.shareData = slider.value
     }
     
     //viewWillAppearを使ってdelegateの値を取得する
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        self.slider.value = delegate.shareData
-        self.label.text = "\(self.slider.value)"
+        slider.value = delegate.shareData
+        label.text = "\(slider.value)"
     }
 }
